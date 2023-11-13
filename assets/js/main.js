32408cf6d698b11a15221723f298b166adaac899
bmistry4 <mistrybhk@gmail.com>
2023-11-13 22:54:13 +0000

    update html with minor typo fixes and update css using final page styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,7 +73,6 @@ var CollapsibleModule = (function () {
 // Usage: Initialize the Collapsible Module
 CollapsibleModule.initialize();
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // window.addEventListener('scroll', function () {
@@ -89,39 +88,3 @@ CollapsibleModule.initialize();
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-/**
- * Mobile nav toggle
- */
-on('click', '.mobile-nav-toggle', function (e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-})
-
-/**
- * Mobile nav dropdowns activate
- */
-on('click', '.navbar .dropdown > a', function (e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-        e.preventDefault()
-        this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-}, true)
-
-/**
- * Scrool with ofset on links with a class name .scrollto
- */
-on('click', '.scrollto', function (e) {
-    if (select(this.hash)) {
-        e.preventDefault()
-
-        let navbar = select('#navbar')
-        if (navbar.classList.contains('navbar-mobile')) {
-            navbar.classList.remove('navbar-mobile')
-            let navbarToggle = select('.mobile-nav-toggle')
-            navbarToggle.classList.toggle('bi-list')
-            navbarToggle.classList.toggle('bi-x')
-        }
-        scrollto(this.hash)
-    }
-}, true)
